26257c8d3600b84543bb6eff0b96ed875e14fb7d
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2018-07-17 17:03:59 +0700

    Initial Commit

---
+++ CoreDataDemo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataDemo2
+//
+//  Created by Farhan Yuda Pahlevi on 17/07/18.
+//  Copyright © 2018 Farhan Yuda Pahlevi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
